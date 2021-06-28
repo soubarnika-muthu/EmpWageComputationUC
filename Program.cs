43c28b41a32dd9dbc8a6cb406c5247d802bcadc9
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-06-28 15:39:45 +0530

    Added UC1 program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace EmployeeWagecomputaion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome!");
+            Random random = new Random();
+            int EMP_PRESENT = 1;
+            int EMP_INPUT = random.Next(0,2);
+            if (EMP_INPUT == EMP_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
