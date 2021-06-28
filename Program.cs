fbf8d941159fc27796db1e3c740b1f561fe4fa31
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-06-28 15:22:32 +0530

    Displaying Welcome on master

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWagecomputaion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!");
+        }
+    }
+}
