5c9cc1d9d2443d216dd6df31b8c82f7c361b3ed8
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-06-28 16:10:25 +0530

    Added UC2 Program

--- Program.cs
+++ Program.cs
@@ -7,16 +7,22 @@ namespace EmployeeWagecomputaion
         static void Main(string[] args)
         {
             Random random = new Random();
+            int EMP_WAGE = 0;
             int EMP_PRESENT = 1;
             int EMP_INPUT = random.Next(0,2);
+            int FULL_TIME = 0;
+            int WAGE_PER_HR = 20;
             if (EMP_INPUT == EMP_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
+                FULL_TIME = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
+            EMP_WAGE = FULL_TIME * WAGE_PER_HR;
+            Console.WriteLine("Daily Employee Wage is" + EMP_WAGE);
         }
     }
 }
