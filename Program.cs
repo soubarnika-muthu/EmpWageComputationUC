08e431865a31ca43a781156ff121c83551e6d508
soubarnika-muthu <66513309+soubarnika-muthu@users.noreply.github.com>
2021-06-29 16:14:50 +0530

    updated UC2 program

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ namespace EmployeeWagecomputaion
     {
         static void Main(string[] args)
         {
+            ////Creating object or Instance of random class
             Random random = new Random();
+            //iNitializing variable
             int EMP_WAGE = 0;
             int EMP_PRESENT = 1;
             int EMP_INPUT = random.Next(0,2);
             int FULL_TIME = 0;
             int WAGE_PER_HR = 20;
+            //Using If condition checking whether employee is present or not 
+            
             if (EMP_INPUT == EMP_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
@@ -21,6 +25,7 @@ namespace EmployeeWagecomputaion
             {
                 Console.WriteLine("Employee is Absent");
             }
+            ////Calculating Daily Wages of Employee
             EMP_WAGE = FULL_TIME * WAGE_PER_HR;
             Console.WriteLine("Daily Employee Wage is" + EMP_WAGE);
         }
