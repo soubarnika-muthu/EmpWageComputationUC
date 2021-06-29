7582f8b8de835cd621a7befce682a7cd09793d33
soubarnika-muthu <66513309+soubarnika-muthu@users.noreply.github.com>
2021-06-29 16:12:37 +0530

    updated UC1 program

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ namespace EmployeeWagecomputaion
     {
         static void Main(string[] args)
         {
+           ////Creating object or Instance of random class
             Random random = new Random();
+           //Initializing variables
             int EMP_PRESENT = 1;
             int EMP_INPUT = random.Next(0,2);
+            //Using if condtion to check Employee attendance
             if (EMP_INPUT == EMP_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
